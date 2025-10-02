0b3b5dbc4e2e590ba8433acb890d6c49a5d446c7
Aaron K <aaron.kim.dev@gmail.com>
2025-10-01 23:22:31 -0700

    Search for products

--- products/views.py
+++ products/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
+from django.db.models import Q
 from .models import Product
 
 
 def product_list(request):
     products = Product.objects.all()
 
+    search_query = request.GET.get("search")
+    if search_query:
+        products = products.filter(
+            Q(name__icontains=search_query) | Q(description__icontains=search_query)
+        )
+
     context = {
         "products": products,
+        "search_query": search_query or "",
     }
 
     return render(request, "products/product_list.html", context)
