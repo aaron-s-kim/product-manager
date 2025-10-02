5f51b1228057ffdd4a049cc1d8d4d5a40c3f62a6
Aaron K <aaron.kim.dev@gmail.com>
2025-10-01 23:46:29 -0700

    Filter by Category

--- products/views.py
+++ products/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.db.models import Q
-from .models import Product
+from .models import Product, Category
 
 
 def product_list(request):
     products = Product.objects.all()
+    categories = Category.objects.all()
 
     search_query = request.GET.get("search")
     if search_query:
@@ -12,9 +13,15 @@ def product_list(request):
             Q(name__icontains=search_query) | Q(description__icontains=search_query)
         )
 
+    category_filter = request.GET.get("category")
+    if category_filter:
+        products = products.filter(category_id=category_filter)
+
     context = {
         "products": products,
+        "categories": categories,
         "search_query": search_query or "",
+        "selected_category": category_filter or "",
     }
 
     return render(request, "products/product_list.html", context)
