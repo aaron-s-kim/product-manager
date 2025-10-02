104d97ae9b9a5e72ae389e06af2a96a5e6046a35
Aaron K <aaron.kim.dev@gmail.com>
2025-10-01 20:39:54 -0700

    add django-debug-toolbar

--- project/settings.py
+++ project/settings.py
@@ -31,10 +31,15 @@ DEBUG = env("DEBUG")
 
 ALLOWED_HOSTS = []
 
+# Debug Toolbar settings
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
 
 # Application definition
 
 INSTALLED_APPS = [
+    'debug_toolbar',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -46,6 +51,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
