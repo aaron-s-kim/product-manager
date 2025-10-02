87dcdcf934856751d976af92795ac6834a8afcdf
Aaron K <aaron.kim.dev@gmail.com>
2025-10-01 21:22:07 -0700

    models for category, tag, product, and migration

--- products/models.py
+++ products/models.py
@@ -1,3 +1,72 @@
 from django.db import models
+from django.core.validators import MinValueValidator
+from decimal import Decimal
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = "Categories"
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = "Tags"
+        ordering = ["name"]
+
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        validators=[
+            MinValueValidator(Decimal("0.00")),
+        ],
+    )
+    quantity = models.PositiveIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    # Many products to 1 category
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="products"
+    )
+    # Many products to many tags
+    tags = models.ManyToManyField(
+        Tag,
+        blank=True,
+        related_name="products"
+    )
+
+    class Meta:
+        verbose_name_plural = "Products"
+        ordering = ["-created_at"]  # newest first
+
+    def __str__(self):
+        return self.name
+
+    @property
+    def tag_names(self):
+        return ", ".join([tag.name for tag in self.tags.all()])
+
+    def is_in_stock(self):
+        return self.quantity > 0
