d635f231e7280cc0afc804ab0690402a77f6c6db
Aaron K <aaron.kim.dev@gmail.com>
2025-10-01 23:09:07 -0700

    Display all products

--- products/views.py
+++ products/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from .models import Product
 
 
 def product_list(request):
+    products = Product.objects.all()
 
-    return render(request, "products/product_list.html")
+    context = {
+        "products": products,
+    }
+
+    return render(request, "products/product_list.html", context)
